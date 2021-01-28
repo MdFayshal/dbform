e2bcfa7632557234b19e4a0a1c9316258257309b
shabbir <shabbir.mdfayshal01@gmail.com>
2021-01-29 02:14:01 +0600

    this is 2nd final

--- edit.php
+++ edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('header.php');
 require_once('database.php');
 
@@ -13,7 +13,7 @@ $e  = $_GET['em'];
 <div class="container my-5 ">
     <div class="jumbotron bg-dark py-5 ">
         <h1 class="display-4 text-center text-light bg-info py-3 my-5">EditForm</h1>
-        <form class="form-horizontal offset-3" action="" method="POST" name="form">
+        <form class="form-horizontal offset-3" action="edit.php" method="GET" name="form">
         <div class="row mb-3">
                 <label class="col-sm-2 col-form-label ">ID-Number</label>
                 <div class="col-sm-6">
@@ -23,24 +23,24 @@ $e  = $_GET['em'];
         <div class="row mb-3">
                 <label class="col-sm-2 col-form-label ">First Name</label>
                 <div class="col-sm-6">
-                <input type="text" class="form-control" name="first_name" value="<?php echo $n1;?>">
+                <input type="text" class="form-control" name="fn" value="<?php echo $n1;?>">
                 </div>
             </div>
             <div class="row mb-3">
                 <label  class="col-sm-2 col-form-label">Last Name</label>
                 <div class="col-sm-6">
-                <input type="text" class="form-control" name="last_name" value="<?php echo $n2;?>">
+                <input type="text" class="form-control" name="ln" value="<?php echo $n2;?>">
                 </div>
             </div>
             <div class="row mb-3">
                 <label  class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-6">
-                <input type="email" class="form-control" name="email" placeholder="<?php echo $e;?>">
+                <input type="email" class="form-control" name="em" value="<?php echo $e;?>">
                 </div>
             </div>
             <div class="offset-2">
-            <a href=""type="submit" name="submit" class="btn btn-info text-light mr-2">Update</a>
-            <button type="reset" class="btn btn-info text-light ">Cancel</button>
+            <button type="submit" name="submit" class="btn btn-info text-light mr-2">Update</button>
+            <button type="reset" class="btn btn-info text-light ">Reset</button>
             <a href="all_info.php"type="submit"  class="btn btn-info text-light">Show All</a>
             </div>
         </form>
@@ -51,20 +51,24 @@ $e  = $_GET['em'];
 </div>
 
 <?php
-if($_GET["submit"]){
+if(isset($_GET["submit"])){
+    $name1  = $_GET['fn'];
+    $name2  = $_GET['ln'];
+    $email  = $_GET['em'];
+    $id     = $_GET['id'];
 
-    echo $name1  = $_GET['ufirst_name'];
-    echo $name2  = $_GET['ulast_name'];
-    echo $email   = $_GET['uemail'];
 
-    $query = "UPDATE information SET first_name = '$name1', last_name = '$name2', email = '$email' WHERE id = $id";
-    $updated = mysqli_query($connection,$query);
+    $update = "UPDATE information SET first_name = '$name1', last_name = '$name2', email = '$email' WHERE id = $id";
+    $updated = mysqli_query($connection,$update);
 
     if($updated){
-        echo "--------------------------------- Updated";
+        
+        echo "<script>alert('Update Done.')</script>";
+        
     }else{
         echo "--------------------------------File To Updated";
     }
+                             
 }
 
 ?>
